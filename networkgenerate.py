be8e9fc5347c7e2d7b3f9eea611d57a5c4ef3247
Suhwan Lee <ghksdl6025.gmail.com>
2020-04-24 14:32:43 +0900

    Update network generator

--- networkgenerate.py
+++ networkgenerate.py
@@ -2,6 +2,7 @@ import networkx
 import matplotlib.pyplot as plt
 import math
 import oyaml as yaml
+from argparse import ArgumentParser
 '''
 Erdős–Rényi model
 n is number of nodes
@@ -9,8 +10,11 @@ Almost every graph in G(n, 2ln(n)/n) is connected.
 As n tends to infinity, the probability that a graph on n vertices with edge probability 2ln(n)/n is connected, tends to 1.
 https://en.wikipedia.org/wiki/Erd%C5%91s%E2%80%93R%C3%A9nyi_model
 '''
+parser = ArgumentParser()
+parser.add_argument('-n',default=20, type=int, help='Number of nodes')
+args = parser.parse_args()
 
-n = 4
+n = args.n
 p = 2*math.log(n)/n
 
 graphconnected=True
@@ -30,14 +34,14 @@ with open('./docker-compose.yml') as ymlfile:
 for t in list(data['services'].keys()):
     if 'node' in t:
         del data['services'][t]
-print(data['services'])
 
-for nodeindex in range(1,n+1):
+
+for nodeindex in range(n):
     nodename = 'node'+str(nodeindex)
     port = str(8000+nodeindex)+':5000'
     ipaddress = '172.28.1.%s'%(nodeindex)
 
-    if nodeindex ==1:       
+    if nodeindex ==0:       
         node = {nodename: 
                         {'build': {'context': '.','dockerfile': './compose/webapp/Dockerfile-node'}, 
                         'ports': [port], 
@@ -87,4 +91,5 @@ networkx.draw(graph,with_labels=True)
 
 ax.set_title('Network Structure',fontsize=20)
 # plt.show()
+
 plt.savefig('./networkstructure.png',bbox_inches = "tight")
